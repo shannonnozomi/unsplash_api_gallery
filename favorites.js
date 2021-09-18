5c7aac156725985d2472959def5d3b498581dc31
Shannon Nicholas <shannonnozomi@gmail.com>
2021-09-18 14:18:20 -0400

    prettier files

--- favorites.js
+++ favorites.js
@@ -1,65 +1,68 @@
+var imagesStrings = localStorage.getItem("images");
+var parsedImages = JSON.parse(imagesStrings);
+const divForImg = document.getElementById("favoritesDiv");
+const messageDiv = document.getElementById("message");
 
-    var imagesStrings = localStorage.getItem("images");
-    var parsedImages = JSON.parse(imagesStrings);
-    const divForImg = document.getElementById('favoritesDiv');
-    const messageDiv = document.getElementById("message");
+document.getElementById("clearButton").addEventListener("click", clearFunction);
 
-document.getElementById("clearButton").addEventListener('click', clearFunction);
+var favImagesArray = JSON.parse(localStorage.getItem("images")) || [];
 
-var favImagesArray = JSON.parse(localStorage.getItem('images')) || [];
-
-function createFavImage(newImage){
-    messageDiv.innerHTML="";
-    const imageDiv = document.createElement("div");
-    const image = document.createElement("img");
-    const favoriteIcon = document.createElement("button");
-    image.src = newImage;
-    favoriteIcon.innerHTML ="<span class='heartActive heartFull iconify' data-icon='dashicons:heart' data-inline='false'></span>"
-    favoriteIcon.onclick = function(){favorited(imageDiv, newImage)};
-    image.style.margin = "20px";
-    image.style.width ="400px";
-    image.style.height = "auto";
-    image.style.border = "double 4px black";
-    imageDiv.append(image);
-    divForImg.append(imageDiv);
-    imageDiv.append(favoriteIcon);
-};
+function createFavImage(newImage) {
+  messageDiv.innerHTML = "";
+  const imageDiv = document.createElement("div");
+  const image = document.createElement("img");
+  const favoriteIcon = document.createElement("button");
+  image.src = newImage;
+  favoriteIcon.innerHTML =
+    "<span class='heartActive heartFull iconify' data-icon='dashicons:heart' data-inline='false'></span>";
+  favoriteIcon.onclick = function () {
+    favorited(imageDiv, newImage);
+  };
+  image.style.margin = "20px";
+  image.style.width = "400px";
+  image.style.height = "auto";
+  image.style.border = "double 4px black";
+  imageDiv.append(image);
+  divForImg.append(imageDiv);
+  imageDiv.append(favoriteIcon);
+}
 
 function favorited(imageDiv, newImage) {
-    imageDiv.animate(
-        [
-            {opacity:"-1"}
-         ],
-          {duration:250});
-    function timeFunction() {
-        setTimeout(function(){imageDiv.remove();}, 200);
+  imageDiv.animate([{ opacity: "-1" }], { duration: 250 });
+  function timeFunction() {
+    setTimeout(function () {
+      imageDiv.remove();
+    }, 200);
+  }
+  timeFunction();
+  if (favImagesArray.includes(newImage)) {
+    const index = favImagesArray.indexOf(newImage);
+    if (index > -1) {
+      favImagesArray.splice(index, 1);
     }
-timeFunction();
-if (favImagesArray.includes(newImage)) {
-    const index = favImagesArray.indexOf(newImage)
-    if (index > -1) {favImagesArray.splice(index,1)}
+  }
 }
-};
 
-parsedImages.forEach((newImage)=>{
-    createFavImage(newImage);
+parsedImages.forEach((newImage) => {
+  createFavImage(newImage);
 });
 
-function clearFunction(){
-    if (favImagesArray === undefined || favImagesArray.length == 0) { 
-        console.log("nothing should happen")}
-        else{
-        divForImg.animate(
-        [
-            {opacity:"-1"}
-         ],
-          {duration:250});
+function clearFunction() {
+  if (favImagesArray === undefined || favImagesArray.length == 0) {
+    console.log("nothing should happen");
+  } else {
+    divForImg.animate([{ opacity: "-1" }], { duration: 250 });
     function timeFunction() {
-        setTimeout(function(){divForImg.remove();}, 200);
+      setTimeout(function () {
+        divForImg.remove();
+      }, 200);
     }
     timeFunction();
-    localStorage.clear();}
-};
+    localStorage.clear();
+  }
+}
 
-function favoritePage() {localStorage.setItem("images", JSON.stringify(favImagesArray))};
+function favoritePage() {
+  localStorage.setItem("images", JSON.stringify(favImagesArray));
+}
 document.getElementById("home").addEventListener("click", favoritePage);
--- index.js
+++ index.js
@@ -1,67 +1,78 @@
-const divForImg = document.getElementById('img-box');
+const divForImg = document.getElementById("img-box");
 const favDivForImg = document.getElementById("favoritesDiv");
 
 function makeRequestToUnsplash(requestUrl) {
-    fetch(requestUrl)
-        .then(res=> res.json())
-        .then((data)=>{
-            console.log(data.results)
-            data.results.forEach((imageObj)=>{
-                createImage(imageObj);
-            });
-        });
+  fetch(requestUrl)
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data.results);
+      data.results.forEach((imageObj) => {
+        createImage(imageObj);
+      });
+    });
 }
 
-var favImagesArray = JSON.parse(localStorage.getItem('images')) || [];
+var favImagesArray = JSON.parse(localStorage.getItem("images")) || [];
 
 function favorited(imageId, favoriteIcon) {
-favoriteIcon.classList.toggle("heartActive");
+  favoriteIcon.classList.toggle("heartActive");
 
-if (favImagesArray.includes(imageId)) {
-    const index = favImagesArray.indexOf(imageId)
-    if (index > -1) {favImagesArray.splice(index,1)}
-}
-else {
+  if (favImagesArray.includes(imageId)) {
+    const index = favImagesArray.indexOf(imageId);
+    if (index > -1) {
+      favImagesArray.splice(index, 1);
+    }
+  } else {
     favImagesArray.push(imageId);
-}
-console.log(favImagesArray);
+  }
+  console.log(favImagesArray);
 }
 
-function createImage(imageObj){
-    const imageDiv = document.createElement("div");
-    const image = document.createElement("img");
-    const favoriteIcon = document.createElement("button");
-    favoriteIcon.innerHTML ="<span class='heart heartFull iconify' data-icon='dashicons:heart' data-inline='false'></span>"
-    favoriteIcon.onclick = function(){favorited(imageObj.urls.full, favoriteIcon.firstChild)};
-    image.src = imageObj.urls.regular;
-    image.alt = imageObj.alt_description;
-    image.style.margin = "20px";
-    image.style.width ="400px";
-    image.style.height = "auto";
-    image.style.border = "double 4px black";
-    imageDiv.append(image);
-    divForImg.append(imageDiv);
-    imageDiv.append(favoriteIcon);
-};
+function createImage(imageObj) {
+  const imageDiv = document.createElement("div");
+  const image = document.createElement("img");
+  const favoriteIcon = document.createElement("button");
+  favoriteIcon.innerHTML =
+    "<span class='heart heartFull iconify' data-icon='dashicons:heart' data-inline='false'></span>";
+  favoriteIcon.onclick = function () {
+    favorited(imageObj.urls.full, favoriteIcon.firstChild);
+  };
+  image.src = imageObj.urls.regular;
+  image.alt = imageObj.alt_description;
+  image.style.margin = "20px";
+  image.style.width = "400px";
+  image.style.height = "auto";
+  image.style.border = "double 4px black";
+  imageDiv.append(image);
+  divForImg.append(imageDiv);
+  imageDiv.append(favoriteIcon);
+}
 
 function myFunction() {
-    document.getElementById("img-box").innerHTML = "";
-    var inputVal = document.getElementById("myInput").value;
-    const requestOne = "https://api.unsplash.com/search/photos?page=1&query=" + inputVal + "&client_id=dDm5YTPapDT4uWpO9Rsrt1fnKj7AvR_yayArid0CcSA";
-    const requestTwo = "https://api.unsplash.com/search/photos?page=2&query=" + inputVal + "&client_id=dDm5YTPapDT4uWpO9Rsrt1fnKj7AvR_yayArid0CcSA";
-    makeRequestToUnsplash(requestOne);
-    makeRequestToUnsplash(requestTwo);
+  document.getElementById("img-box").innerHTML = "";
+  var inputVal = document.getElementById("myInput").value;
+  const requestOne =
+    "https://api.unsplash.com/search/photos?page=1&query=" +
+    inputVal +
+    "&client_id=dDm5YTPapDT4uWpO9Rsrt1fnKj7AvR_yayArid0CcSA";
+  const requestTwo =
+    "https://api.unsplash.com/search/photos?page=2&query=" +
+    inputVal +
+    "&client_id=dDm5YTPapDT4uWpO9Rsrt1fnKj7AvR_yayArid0CcSA";
+  makeRequestToUnsplash(requestOne);
+  makeRequestToUnsplash(requestTwo);
 }
 
-document.getElementById("submit").addEventListener('click', myFunction);
+document.getElementById("submit").addEventListener("click", myFunction);
 
-document.getElementById("frm1").addEventListener("keyup", function(e){
-    if (e.key === "Enter") {
-       myFunction();
-    }
+document.getElementById("frm1").addEventListener("keyup", function (e) {
+  if (e.key === "Enter") {
+    myFunction();
+  }
 });
 
-function favoritePage() {localStorage.setItem("images", JSON.stringify(favImagesArray))};
+function favoritePage() {
+  localStorage.setItem("images", JSON.stringify(favImagesArray));
+}
 
 document.getElementById("favs").addEventListener("click", favoritePage);
-
